550d15a6c6575ede33f7c7998dda248f535d1c9e
Dmytro <Fedorchukdima1999@gmail.com>
2026-05-06 00:15:01 +0300

    Додав усі математичні функції та тести

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
 def multiply(a, b):
     return a * b
 
-if __name__ == "__main__":
-    print(f"Результат: {multiply(10, 2)}")
+def divide(a, b):
+    if b == 0:
+        return "Помилка: ділення на нуль!"
+    return a / b
 
+if __name__ == "__main__":
+    print("--- Математичний модуль готовий ---")
+    print(f"1 + 1 = {add(1, 1)}")
+    print(f"10 - 5 = {subtract(10, 5)}")
+    print(f"3 * 4 = {multiply(3, 4)}")
+    print(f"20 / 4 = {divide(20, 4)}")
--- test_app.py
+++ test_app.py
@@ -1,10 +1,19 @@
 import unittest
-from app import multiply
+from app import add, subtract, multiply, divide
 
-class TestMath(unittest.TestCase):
+class TestMathApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 1), 2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+
     def test_multiply(self):
-        self.assertEqual(multiply(3, 5), 15)
-        self.assertEqual(multiply(-1, 5), -5)
+        self.assertEqual(multiply(3, 4), 12)
 
-if __name__ == '__main__':
+    def test_divide(self):
+        self.assertEqual(divide(20, 4), 5)
+        self.assertEqual(divide(5, 0), "Помилка: ділення на нуль!")
+
+if __name__ == "__main__":
     unittest.main()
